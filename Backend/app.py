2ad44db70deea3ac382f5025e8787c75d128c98e
2730538920101 <2730538920101@ingenieria.usac.edu.gt>
2022-08-01 17:47:29 -0600

    (add) analizador lexico

--- Backend/app.py
+++ Backend/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import ply.lex as lex
+from analizador.parser.gramatica import *
+lexer = lex.lex()
 
 app = Flask(__name__)
 CORS(app)
@@ -11,7 +14,14 @@ def ping():
 @app.route('/analizar', methods=['POST'])
 def Analizar():
     aux = request.json
-    print(aux['code'])
+    entrada = aux['code']
+    lexer.input(entrada) 
+    while True:
+        tok = lexer.token()
+        if not tok: 
+            break      # No more input
+        print(tok)   
+    print(entrada)
     return jsonify({"message":"SUCCESS"})
 
 if __name__ == '__main__':
